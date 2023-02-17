7903a6f1589688d723784fc3551f812133d20af8
eyobelakew <eyobelakew4@gmail.com>
2023-02-17 20:39:27 +0300

    commited

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /**
 * main - prints all possible different combinations of three digits
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
 int n, m, i;
-
 for (n = 48; n < 58; n++)
 for (m = n + 1; m < 58; m++)
 for (i = m + 1; i < 58; i++)
@@ -22,8 +19,6 @@ putchar(',');
 putchar(' ');
 }
 }
-
 putchar('\n');
-
 return (0);
 }
