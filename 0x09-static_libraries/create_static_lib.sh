4458aee3630adaca339ab5ba1cf7636adbf4e564
eyobelakew <Eyobelakew4@gmail.com>
2023-03-10 21:24:01 +0300

    0x09 project

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-gcc -c *.c
+
+# Compile all C files in the current directory into object files with warnings enabled
+gcc -Wall -Werror -Wextra -pedantic -c *.c
+
+# Create a static library named liball.a from the object files
 ar -rc liball.a *.o
+
+# Index the library for faster linking
 ranlib liball.a
+
+# Clean up the object files
+rm *.o
+
+# Print a success message
+echo "Static library liball.a has been created."
