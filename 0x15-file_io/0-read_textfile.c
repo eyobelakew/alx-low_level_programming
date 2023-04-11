4d547701e6c59d0512b0ca1659bb8a03affb564e
eyobelakew <Eyobelakew4@gmail.com>
2023-04-11 19:02:36 +0300

    bug fixing task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,61 +1,114 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
-* read_textfile - function that reads text file and prints to standard output
-* @filename: text file
-* @letters: number of letters it should read and print
-* Description: 0. Tread lightly, she is near
-* Return: see below
-* 1. if successful, actual number of letters
-* 2. if fail because file can't be opened or read, 0
-*/
-
-/**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * read_textfile - Reads a text file and prints it to the POSIX standard output
+ * @filename: Pointer to the name of the file to read
+ * @letters: Number of letters it should read and print
  *
- * @filename: The name of the file to read.
- * @letters: The number of letters to read and print.
- *
- * Return: The actual number of letters read and printed, or 0 if an error occurred.
+ * Return: The actual number of letters read and printed, or 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t n, m;
-    char *buffer;
     int fd;
+    ssize_t nread;
+    char *buf;
 
-    if (filename == NULL)
-        return (0);
-
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
+    if (!filename)
         return (0);
 
     fd = open(filename, O_RDONLY);
     if (fd == -1)
+        return (0);
+
+    buf = malloc(sizeof(char) * (letters + 1));
+    if (!buf)
+        return (0);
+
+    nread = read(fd, buf, letters);
+    if (nread == -1)
     {
-        free(buffer);
+        free(buf);
         return (0);
     }
 
-    n = read(fd, buffer, letters);
-    if (n == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+    buf[nread] = '\0';
+    printf("%s", buf);
 
-    m = write(STDOUT_FILENO, buffer, n);
-    if (m == -1 || m != n)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
-
-    free(buffer);
+    free(buf);
     close(fd);
 
-    return (n);
+    return (nread);
+}
+
+/**
+ * create_file - Creates a file and writes text to it
+ * @filename: Pointer to the name of the file to create
+ * @text_content: Pointer to a null terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+    int fd;
+    ssize_t nwrite = 0, len = 0;
+
+    if (!filename)
+        return (-1);
+
+    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+    if (fd == -1)
+        return (-1);
+
+    if (text_content)
+    {
+        while (text_content[len])
+            len++;
+
+        nwrite = write(fd, text_content, len);
+        if (nwrite == -1 || nwrite != len)
+        {
+            close(fd);
+            return (-1);
+        }
+    }
+
+    close(fd);
+    return (1);
+}
+
+/**
+ * append_text_to_file - Appends text to the end of a file
+ * @filename: Pointer to the name of the file to append to
+ * @text_content: Pointer to a null terminated string to append to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+    int fd;
+    ssize_t nwrite = 0, len = 0;
+
+    if (!filename)
+        return (-1);
+
+    fd = open(filename, O_WRONLY | O_APPEND);
+    if (fd == -1)
+        return (-1);
+
+    if (text_content)
+    {
+        while (text_content[len])
+            len++;
+
+        nwrite = write(fd, text_content, len);
+        if (nwrite == -1 || nwrite != len)
+        {
+            close(fd);
+            return (-1);
+        }
+    }
+
+    close(fd);
+    return (1);
 }
